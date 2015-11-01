16116ec2e36c0ff997c702c09b3ef36e5fce3ce3
enkiv2 <john.ohno@gmail.com>
2015-10-31 21:49:16 -0500

    use md

--- orgasmotron.sh
+++ orgasmotron.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env zsh
-while [[ $(wc -w < orgasmotron.txt) -lt 50000 ]] ; do  ggc orgasmotron.gg | python2 >> orgasmotron.txt; done
+while [[ $(wc -w < orgasmotron.md) -lt 50000 ]] ; do  ggc orgasmotron.gg | python2 >> orgasmotron.md; done
