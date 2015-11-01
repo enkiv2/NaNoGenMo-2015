73cbec4ca44d0677188051df13669f777db8882d
enkiv2 <john.ohno@gmail.com>
2015-10-31 21:51:29 -0500

    add extra whitespace

--- orgasmotron.sh
+++ orgasmotron.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 while [[ $(wc -w < orgasmotron.md) -lt 50000 ]] ; do  ggc orgasmotron.gg | python2 >> orgasmotron.md; done
-sed 's/^$/\n/g' -i orgasmotron.md 
+sed 's/$/\n/g' -i orgasmotron.md 
